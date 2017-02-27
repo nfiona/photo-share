fda630fd85a00b738a79d84fbd8d98c666735de8
Fiona <h.natapra@gmail.com>
2017-02-26 21:01:14 -0500

    added seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+Photo.destroy_all
+Album.destroy_all
+
+children = Album.create(title: "Children's Art Festival")
+fash = Album.create(title: "Mint Lola")
+
+children_one = Photo.create(img_url: "https://flic.kr/p/vA1Da4", description: "children tinkering..", album: children)
+children_two = Photo.create(img_url: "https://flic.kr/p/vA3hWV", description: "dancers", album: children)
+fash_one = Photo.create(img_url: "https://flic.kr/p/zbcnyi", description: "Mint Lola studio, DC", album: fash)
+fash_two = Photo.create(img_url: "https://flic.kr/p/N43C4E", description: "Mint Lola at Crafty Bastard, DC", album: fash)
+
+Comment.create(body: "Test Comment", photo: children_one)
+Comment.create(body: "Test Comment", photo: fash_one)
