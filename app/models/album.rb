f85d42a33684e884854b2b8bd3f5700b70aaa579
Fiona <h.natapra@gmail.com>
2017-02-27 16:59:02 -0500

    migrate add_users_to_albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
   has_many :photos, dependent: :destroy
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227213522) do
+ActiveRecord::Schema.define(version: 20170227215616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170227213522) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "img_url"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_albums_on_user_id", using: :btree
   end
 
   create_table "comments", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 20170227213522) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "albums", "users"
 end
