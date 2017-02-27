232b33f327f4d3b137f443898774aa72363d6c4a
Fiona <h.natapra@gmail.com>
2017-02-27 11:57:48 -0500

    CRUD comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,39 @@
 class CommentsController < ApplicationController
 
-
-
-
-
-
-
-
-
+  def new
+      @album = Album.find(params[:album_id])
+      @photo = Photo.find(params[:photo_id])
+      @comment = Comment.new
+    end
+    def create
+      @album = Album.find(params[:album_id])
+      @photo = Photo.find(params[:photo_id])
+      @comment = @photo.comments.create(comment_params)
+      redirect_to album_photo_path(@album,@photo)
+    end
+    def edit
+      @album = Album.find(params[:album_id])
+      @photo = Photo.find(params[:photo_id])
+      @comment = Comment.find(params[:id])
+    end
+    def update
+      @album = Album.find(params[:album_id])
+      @photo = Photo.find(params[:photo_id])
+      @comment = Comment.find(params[:id])
+      @comment.update(comment_params)
+      redirect_to album_photo_path(@photo)
+    end
+    def destroy
+      @album = Album.find(params[:album_id])
+      @photo = Photo.find(params[:photo_id])
+      @comment = Comment.find(params[:id])
+      @comment.destroy
+      redirect_to album_photo_path(@album,@photo)
+    end
+    private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 
 
 
